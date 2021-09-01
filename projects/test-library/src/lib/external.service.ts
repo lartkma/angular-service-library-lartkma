15d65a45260c0d854c1cfc973120a54c43496675
Luis Ramirez <lartkma@gmail.com>
2021-08-31 19:31:53 -0500

    Original template (that causes error)

--- projects/test-library/src/lib/external.service.ts
+++ projects/test-library/src/lib/external.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'any'
+  providedIn: 'root'
 })
 export class ExternalService {
 
